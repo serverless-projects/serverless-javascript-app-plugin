b73e905f05be270dc168a6d7870cb18cf82ce70a
Omar Shibli <omarshib@gmail.com>
2017-11-12 22:46:06 +0200

    added purge cache task

--- index.js
+++ index.js
@@ -19,11 +19,18 @@ class ServerlessPlugin {
           'domainInfo',
         ],
       },
+      purgeCache: {
+        usage: 'Purge CloudFront distribution',
+        lifecycleEvents: [
+          'purgeCache',
+        ],
+      },
     };
 
     this.hooks = {
       'syncToS3:sync': this.syncDirectory.bind(this),
       'domainInfo:domainInfo': this.domainInfo.bind(this),
+      'purgeCache:purgeCache': this.purgeCache.bind(this),
     };
   }
 
@@ -67,9 +74,51 @@ class ServerlessPlugin {
         const outputs = result.Stacks[0].Outputs;
         const output = outputs.find(entry => entry.OutputKey === 'WebAppCloudFrontDistributionOutput');
         if (output.OutputValue) {
-          this.serverless.cli.log(`Web App Domain: ${output.OutputValue}`);
+            this.serverless.cli.log(`Web App Domain: ${output.OutputValue}`);
         } else {
           this.serverless.cli.log('Web App Domain: Not Found');
+        }
+    });
+  }
+
+  purgeCache() {
+    const provider = this.serverless.getProvider('aws');
+    const stackName = provider.naming.getStackName(this.options.stage);
+    return provider
+      .request(
+        'CloudFormation',
+        'describeStacks',
+        { StackName: stackName },
+        this.options.stage,
+        this.options.region // eslint-disable-line comma-dangle
+      )
+      .then((result) => {
+        const outputs = result.Stacks[0].Outputs;
+        const output = outputs.find(entry => entry.OutputKey === 'WebAppCloudFrontDistributionIdOutput');
+        if (output.OutputValue) {
+            const args = [
+                'cloudfront',
+                'create-invalidation',
+                '--distribution-id',
+                output.OutputValue,
+                '--paths',
+                '/*',
+            ];
+
+            const result = spawnSync('aws', args);
+            const stdout = result.stdout.toString();
+            const sterr = result.stderr.toString();
+            if (stdout) {
+            this.serverless.cli.log(stdout);
+            }
+            if (sterr) {
+            this.serverless.cli.log(sterr);
+            }
+            if (!sterr) {
+            this.serverless.cli.log('Successfully created invalidation request');
+            }
+        } else {
+          this.serverless.cli.log('Distribution Not Found');
         }
       });
   }
